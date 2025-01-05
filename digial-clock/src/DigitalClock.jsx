4d3672925566338e11ce2e112662126caf783f6c
Shubham <shubhampatil1356@gmail.com>
2025-01-06 00:59:55 +0530

    Created a simple digital clock using react.

---
+++ digial-clock/src/DigitalClock.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+
+function DigitalClock() {
+
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        }
+    }, [])
+
+
+    function formattedTime() {
+        let hours = time.getHours();
+        const minutes = time.getMinutes();
+        const seconds = time.getSeconds();
+
+        // for 12 hours clock
+        const meridiem = hours < 12 ? "AM" : "PM";
+        hours = hours % 12 || 12; // output: 1 to 11 if(0) then 12
+
+        return (`${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)} ${meridiem}`);
+    }
+
+    function padZero(number) {
+        return (number < 10 ? "0" : "") + number;
+    }
+
+    return (
+        <div className="clock-container">
+            <div className="clock">
+                <span>{formattedTime()}</span>
+            </div>
+        </div>
+    );
+}
+
+export default DigitalClock;
